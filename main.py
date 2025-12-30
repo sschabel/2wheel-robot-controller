2731224cfb2c8a6eccca809948634ea088e79733
Sam Schabel <schabelsam@gmail.com>
2025-12-29 21:04:33 -0500

    Changed to use bluetooth instead of make HTTP GET requests to the robot

--- main.py
+++ main.py
@@ -1,104 +1,85 @@
+import asyncio
+
+import aioble
 from machine import Pin
-import network
-import requests
-import secrets
+import bluetooth
 from utime import sleep_ms
+
+# NUS UUIDs
+_UART_UUID = bluetooth.UUID('6E400001-B5A3-F393-E0A9-E50E24DCCA9E')
+_UART_TX_UUID = bluetooth.UUID('6E400003-B5A3-F393-E0A9-E50E24DCCA9E')
+_UART_RX_UUID = bluetooth.UUID('6E400002-B5A3-F393-E0A9-E50E24DCCA9E')
+
+peripheral_name = "RCCar"
+
+async def find_rc_car():
+    async with aioble.scan(5000, interval_us=30000, window_us=30000, active=True) as scanner:
+        async for result in scanner:
+            if result.name() == peripheral_name and _UART_UUID in result.services():
+                return result.device
+    return None
 
 left_button = Pin(16, Pin.IN, Pin.PULL_UP) # button to turn game on & off
 right_button = Pin(28, Pin.IN, Pin.PULL_UP) # button to turn game on & off
 forward_button = Pin(27, Pin.IN, Pin.PULL_UP) # button to turn game on & off
 reverse_stop_button = Pin(20, Pin.IN, Pin.PULL_UP) # button to turn game on & off
 
-# Used the following Raspberry Pi Article to create the Web Server:
-# https://www.raspberrypi.com/news/how-to-run-a-webserver-on-raspberry-pi-pico-w/
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(secrets.SSID, secrets.PASSWORD)
-
-max_wait = 10
-while max_wait > 0:
-    if wlan.status() < 0 or wlan.status() >= 3:
-        break
-    max_wait -= 1
-    print('Waiting for network connection...')
-    sleep_ms(1000)
-
-if wlan.status() != 3:
-    print(str(wlan.status()) + ' network status.')
-    raise RuntimeError('Network connection failed!')
-else:
-    print('Connected successfully to the network!')
-    status = wlan.ifconfig()
-    print( 'ip = ' + status[0] )
-
-base_uri = "http://" + secrets.ROBOT_ADDRESS
-
-connected = False
-attempts_to_connect = 0
-print("Connecting to robot... beep boop")
-while connected == False:
-    try:
-        attempts_to_connect = attempts_to_connect + 1
-        response = requests.get(base_uri + "/drive/ping")
-        if response.status_code == 200:
-            connected = True
-            print("Connected to robot! Beep boop!")
-    except OSError as e:
-        print("Could not connect to robot...")
-        if attempts_to_connect > 5:
-            raise RuntimeError("Tried connecting to the robot several times and could not! Make sure it is on and try again!")
-        else:
-            print("Attempting to connect again...")
-            sleep_ms(1000)
-
 reverse_button_count = 0
 lastCommand = None
 
-while True:
-    try:
-        if left_button.value() == 0:
-            if lastCommand == "left":
-                sleep_ms(200)
-            print("Left...")
-            lastCommand = "left"
-            response = requests.get(base_uri + "/drive/left")
-            print("Response " + str(response.status_code) + " - " + str(response.content))
+async def main():
+    while True:
+        device = await find_rc_car()
+        if not device:
+            print("RC Car not found. Retrying...")
+            await asyncio.sleep_ms(2000)  # Wait before retrying
+            continue
 
-        if right_button.value() == 0:
-            if lastCommand == "right":
-                sleep_ms(200)
-            print("Right...")
-            lastCommand = "right"
-            response = requests.get(base_uri + "/drive/right")
-            print("Response " + str(response.status_code) + " - " + str(response.content))
+        try:
+            print("Connecting to", device)
+            connection = await device.connect()
+        except asyncio.TimeoutError:
+            print("Connection timeout")
+            continue
 
-        if forward_button.value() == 0:
-            if lastCommand == "forward":
-                sleep_ms(200)
-            print("Forward...")
-            lastCommand = "forward"
-            response = requests.get(base_uri + "/drive/forward")
-            print("Response " + str(response.status_code) + " - " + str(response.content))
+        async with connection:
+            try:
+                uart_service = await connection.service(_UART_UUID)
+                rx_characteristic = await uart_service.characteristic(_UART_RX_UUID)
+            except Exception:
+                print("Failed to find service/characteristic")
+                continue
 
-        if reverse_stop_button.value() == 0:
-            reverse_button_count = reverse_button_count + 1
-            if reverse_button_count > 2:
-                if lastCommand == "reverse":
-                    sleep_ms(200)
-                print("Reverse...")
-                lastCommand = "reverse"
-                response = requests.get(base_uri + "/drive/reverse")
-                print("Response " + str(response.status_code) + " - " + str(response.content))
-            else:
-                if lastCommand == "stop":
-                    sleep_ms(200)
-                print("Stop...")
-                lastCommand = "stop"
-                response = requests.get(base_uri + "/drive/stop")
-                print("Response " + str(response.status_code) + " - " + str(response.content))
-        else:
-            reverse_button_count = 0
-    except OSError as e:
-        print("Error encountered: " + str(e))
+            print("Ready to send commands. Enter: forward, reverse, left, right, stop, or exit")
 
-    sleep_ms(100)
+            cmd = None
+            while True:
+                if left_button.value() == 0:
+                    cmd = 'left'
+                    lastCommand = cmd
+                if right_button.value() == 0:
+                    cmd = 'right'
+                    lastCommand = cmd
+                if forward_button.value() == 0:
+                    cmd = 'forward'
+                    lastCommand = cmd
+                if reverse_stop_button.value() == 0:
+                    reverse_button_count = reverse_button_count + 1
+                    if reverse_button_count > 2:
+                        if lastCommand == "reverse":
+                            sleep_ms(200)
+                        print("Reverse...")
+                        cmd = 'reverse'
+                    else:
+                        if lastCommand == "stop":
+                            sleep_ms(200)
+                        cmd = 'stop'
+                else:
+                    reverse_button_count = 0
+                if cmd is not None:
+                    await rx_characteristic.write(cmd.encode('utf-8'))
+                    print("Sent: ", cmd)
+                    cmd = None
+                sleep_ms(100)
+
+asyncio.run(main())
